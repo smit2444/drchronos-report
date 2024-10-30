d72c50e1410db22e1453a1ec6bdcdc5917502622
smit2444 <157842055+smit2444@users.noreply.github.com>
2024-10-29 22:12:36 -0400

    Update AuthorizationButton.js

--- frontend/src/components/AuthorizationButton.js
+++ frontend/src/components/AuthorizationButton.js
@@ -3,7 +3,7 @@ import { Button } from '@mui/material';
 
 const clientId = 'KPxwLsdr7EBH34tmiTEKAojT2KwyLJI48n9eUP47';
 const redirectUri = 'https://drchronos-report-front.onrender.com/oauth-callback';
-const scope = 'labs:read messages:read patients:read patients:summary:read settings:read tasks:read user:read billing:patient-payment:read billing:read calendar:read clinical:read'; // Adjust the scope based on your needs
+const scope = 'labs:read messages:read patients:read patients:summary:read settings:read tasks:read user:read billing:patient-payment:read billing:read calendar:read clinical:read calendar:write clinical:write billing:write'; // Adjust the scope based on your needs
 
 const authorizationUrl = `https://drchrono.com/o/authorize/?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`;
 
